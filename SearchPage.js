14d7e7688658d10b8a09f0cbe24687cc04e72879
Shian Huang <shian.huang@manheim.com>
2016-05-01 10:19:24 -0400

    Added go functionality

--- SearchPage.js
+++ SearchPage.js
@@ -62,10 +62,94 @@ let styles = StyleSheet.create({
     height: 138
   }
 });
-  
+ 
+function urlForQueryAndPage(key, value, pageNumber) {
+  let data = {
+    country: 'uk',
+    pretty: '1',
+    encoding: 'json',
+    listing_type: 'buy',
+    action: 'search_listings',
+    page: pageNumber
+  };
+  data[key] = value;
+
+  let querystring = Object.keys(data)
+    .map(key => key + '=' + encodeURIComponent(data[key]))
+    .join('&');
+
+  return 'http://api.nestoria.co.uk/api?' + querystring;
+}
 
 class SearchPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchString: 'london',
+      isLoading: false,
+      message: ''
+    }
+  }
+
+  onSearchTextChanged(event) {
+    console.log('onSearchTextChanged');
+    this.setState({ searchString: event.nativeEvent.text  });
+    console.log(this.state.searchString);
+  }
+
+  _executeQuery(query) {
+    console.log(query);
+    this.setState({ isLoading: true  });
+/*
+    fetch(query)
+      .then(response => response.json())
+      .then(json => this._handleResponse(json.response))
+      .catch(error => 
+          this.setState({
+            isLoading: false,
+            message: 'Something bad happened ' + error
+      }));    
+*/
+    fetch(query)
+        .then(response => response.json())
+          .then(json => this._handleResponse(json.response))
+            .catch(error =>
+                this.setState({
+                        isLoading: false,
+                              message: 'Something bad happened ' + error
+                                   
+                }));
+
+  }
+/*
+  _handleResponse(response) {
+    this.setState({ isLoading: false, message: '' });
+    if (response.application_response_code.substr(0,1) === '1') {
+      console.log('Properties found: ' + response.listings.length);
+    }  else {
+      this.setState({ message: 'Location not recognized; please try again.' })
+    }
+  }
+*/
+  _handleResponse(response) {
+    this.setState({ isLoading: false , message: ''  });
+    if (response.application_response_code.substr(0, 1) === '1') {
+      console.log('Properties found: ' + response.listings.length);
+    } else {
+      this.setState({ message: 'Location not recognized; please try again.' });
+    }
+  }
+
+  onSearchPressed() {
+    let query = urlForQueryAndPage('place_name', this.state.searchString, 1);
+    this._executeQuery(query);
+  }
+
   render() {
+    console.log('SearchPage.render');
+    let spinner = this.state.isLoading ? 
+      (<ActivityIndicatorIOS size='large'/>) : (<View/>); 
+
     return (
       <View style={styles.container}>
         <Text style={styles.description}>
@@ -77,8 +161,11 @@ class SearchPage extends Component {
         <View style={styles.flowRight}>
           <TextInput 
             style={styles.searchInput}
+            value={this.state.searchString}
+            onChange={this.onSearchTextChanged.bind(this)}
             placeholder='Search via name or postcode' />
           <TouchableHighlight style={styles.button}
+            onPress={this.onSearchPressed.bind(this)}
             underlayColor='#99d9f4'>
             <Text style={styles.buttonText}>Go</Text>
           </TouchableHighlight>
@@ -88,6 +175,8 @@ class SearchPage extends Component {
             <Text style={styles.buttonText}>Location</Text>
           </TouchableHighlight>
           <Image source={require('./Resources/house.png')} style={styles.image}/>
+          {spinner}
+        <Text style={styles.description}>{this.state.message}</Text>
       </View>
       );
   }
